61e8bc4e568292ee79eb4366330fa37064c85906
sanish <s.karki1994@gmail.com>
2025-06-09 13:46:21 +0545

    In array methods , splice()

--- Index.js
+++ Index.js
@@ -122,3 +122,48 @@ const salad = [
 
 // ------------------------ ARRAY METHODS ------------------------
 // 1) concat(): merges one or more array and return merged array
+// const first = [1, 2, 3];
+// const second = [4, 5, 6];
+// const merged = first.concat(second);
+// console.log(merged);  // [1, 2, 3, 4, 5, 6];
+
+// 2) Join(): Joins all the elements of an array using an SEPERATOR and returns a STRING
+// const fruits = ['🍇', '🍉', '🍓', '🫐'];
+// const joined = fruits.join('<=>');
+// console.log(joined);   // 🍇<=>🍉<=>🍓<=>🫐
+
+// 3) Fill():
+// const colors = ['Red', 'Blue', 'Green', 'Brown', 'Magneta'];
+// colors.fill('Pink', 1, 4);
+// console.log(colors); // ['Pink','Pink','Pink']
+
+// 4) includes():  if element is present in array>>returns TRUE otherwise False
+// const names = ['Ram', 'Hari', 'Sita', 'Raju', 'Tom', 'Bob'];
+// console.log(names.includes('Tom'));
+// console.log(names.includes('john'));
+
+// ✅ 5) indexOf(): when we want of know the index position  if an element, element repeat xa bhane first ma aaune ko index return garxa, find garena bhane returns  -1
+// const names = ['Ram', 'Hari', 'Sita', 'Raju', 'Tom', 'Bob'];
+// console.log(names.indexOf('Raju')); // 3
+// console.log(names.indexOf('Eminem')); // -1
+
+// 6) lastIndexOf(): repeated elements haru ko last wala ko index return garxa
+// const names = ['tom', 'hardy', 'cruise', 'holland', 'brady', 'tom'];
+// console.log(names.lastIndexOf('tom'));
+
+// 7) reverse(): reverse the elements position in an array
+// const names = ['tom', 'hardy', 'cruise', 'holland'];
+// console.log(names.reverse());
+
+// 8) sort(): the default sort methods converts the elements types into strings | the default sorting order is ascending.
+// const names = ['tom', 'hardy', 'cruise', 'holland', 'brady', 'hanks'];
+// console.log('After default sorting:', names.sort()); // By default it sorts in Ascending order
+
+// ✅ 9) splice(): delete, add, modify elements in an array ,from any index in an array
+const names = ['tom', 'alex', 'bob'];
+console.log('Output of splice:', names.splice(0, 1, 'john')); // ['tom'] : 'tom' gets deleted from the array ,so the deleted array is returned
+console.log('Original array becomes:', names); // ['john', 'alex', 'bob']
+
+console.log(names.splice(1, 0, 'Zack')); // OP: ["tom",'zack', 'alex', 'bob']
+
+names.splice();
